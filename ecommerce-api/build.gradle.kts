11ae1cc7fa384f086614c2c4600b6d7c49438b57
Kimchuljoong <wnmh123@gmail.com>
2025-03-10 08:08:22 +0900

    setting logback for logging

--- ecommerce-api/build.gradle.kts
+++ ecommerce-api/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("ch.qos.logback.contrib", "logback-json-classic", "0.1.5")
+    implementation("ch.qos.logback.contrib", "logback-jackson", "0.1.5")
     testImplementation("org.springframework.batch:spring-batch-test")
 }
 
